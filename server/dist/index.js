f0eb52f70b9da4d610cf3145cdc87370a0ef9a48
Qarib Iqbal <qaribiqbal92@gmail.com>
2025-11-11 01:08:16 +0500

    cors error

--- server/dist/index.js
+++ server/dist/index.js
@@ -13,17 +13,32 @@ app.use(helmet({ crossOriginResourcePolicy: false,
     crossOriginOpenerPolicy: false,}));
 const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || ['https://partners.classera.com', 'http://localhost:5173',];
 
+// app.use(cors({
+//     origin: function(origin, callback) {
+//         if (!origin) return callback(null, true); // allow non-browser requests
+//         if (allowedOrigins.includes(origin)) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     },
+//     credentials: true
+// }));
 app.use(cors({
     origin: function(origin, callback) {
-        if (!origin) return callback(null, true); // allow non-browser requests
+        if (!origin) return callback(null, true);
         if (allowedOrigins.includes(origin)) {
             callback(null, true);
         } else {
             callback(new Error('Not allowed by CORS'));
         }
     },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
+app.options('*', cors()); // Handles preflight
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 async function startServer() {
